7c718cfc99549c35a569c7766519a2a86c85b851
Han Htoo Phyo <144544016+hanhtoophyo90@users.noreply.github.com>
2025-07-25 13:02:29 +0800

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Home from './pages/Home';
+import HeritageList from './components/HeritageList';
 
 function App() {
   const { t } = useTranslation();
-  return <div><h1>{t('welcome')}</h1><Home /></div>;
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">{t('welcome')}</h1>
+      
+      {/* Home page component */}
+      <Home />
+
+      {/* Heritage List Map + Card + Filter */}
+      <HeritageList />
+    </div>
+  );
 }
 
 export default App;
